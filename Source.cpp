813180c91943f8cf49116294f5e024de67f8c973
Mr-Gradus <rybin.dmitriy@gmail.com>
2022-01-17 00:11:57 +0500

    Task 1
    task 1 is Done

--- Source.cpp
+++ Source.cpp
@@ -5,32 +5,67 @@
 
 struct Person {
 	
-	std::string surname;
-	std::string name;
-	std::string patronymic;
+	std::string surname{"Rybin"};
+	std::string name{"Ivan"};
+	std::optional<std::string> patronymic {"Dmitrievich"};
 
 };
 
+std::optional<std::string> getPatStr(std::string& pat)
+{
+	if ( pat == "")
+	{
+		return std::nullopt;
+	}
+
+	return pat;
+}
 
 std::ostream& operator<< (std::ostream& out, const Person& person)
 {
-	return out << person.surname << " " << person.name << " " << person.patronymic;
+	out << person.surname << "   " << person.name << "   ";
+
+	if (person.patronymic.has_value())
+	{
+		out << person.patronymic.value();
+	}
+	else
+	{
+		out << "";
+	}
+	return out;
 }
 
+bool operator< (const Person& person1, const Person& person2)
+{
+	return std::make_tuple(person1.surname, person1.name, person1.patronymic) < std::make_tuple(person2.surname, person2.name, person2.patronymic);
+}
+
+bool operator== (const Person& person1, const Person& person2)
+{
+	return std::make_tuple(person1.surname, person1.name, person1.patronymic) == std::make_tuple(person2.surname, person2.name, person2.patronymic);
+}
 
 struct PhoneNumber {
 
 	int pageCode;
 	int cityCode;
 	int phoneNum;
-	int addNum;
+	std::optional<int> addNum;
 };
 
+std::optional<int> getAddNum(int& addNum)
+{
+	if ( addNum == 0)
+	{
+		return std::nullopt;
+	}
 
+	return addNum;
+}
 
 int main()
 {
-
 	Person person;
 	std::cout << person;
 
